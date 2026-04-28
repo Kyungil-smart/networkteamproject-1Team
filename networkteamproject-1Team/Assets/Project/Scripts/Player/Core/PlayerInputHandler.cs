edaf28f37d000d4ceed0964ea51d37531e111856
dev-LS-archive <gyu3941@gmail.com>
2026-04-28 17:18:34 +0900

    feat: PlayerMovement 모듈 구현 및 InputHandler 연결

--- networkteamproject-1Team/Assets/Project/Scripts/Player/Core/PlayerInputHandler.cs
+++ networkteamproject-1Team/Assets/Project/Scripts/Player/Core/PlayerInputHandler.cs
@@ -20,15 +20,19 @@ namespace Player
 #endif
         
         // 하위로 입력을 받는 모듈 선언
-        // 이동
+        private PlayerMovement _movement;
         // 카메라
         // 전투
         // 상호작용
-
+        
+        private bool _lastSprintState;
+        
         // 하위 모듈 생성자 할당 및 초기화
-        public void Initialize()
+        public void Initialize(PlayerMovement move)
         {
             // 생성된 모듈 연결
+            _movement = move;
+            // _camera = c; _combat = cb; _interactor = i;
             BindEvents();
         }
         
@@ -36,7 +40,7 @@ namespace Player
         void BindEvents()
         {
             _input.Enable();
-            // _input.onMove += OnMove;
+             _input.onMove += OnMove;
         }
         
         // 이동 관련 이벤트 할당 해제(메모리 누수 방지)
@@ -44,21 +48,37 @@ namespace Player
         {
             // _input이 없다면 할당 해제할 이벤트도 없으므로 return
             if (_input == null) return;
-            // _input.onMove -= OnMove;
+            _input.onMove -= OnMove;
+            _input.onJump          += OnJump;
+            // _input.onAttack        += OnAttack;
+            // _input.onInteract      += OnInteractStart;
+            // _input.Interact        += OnInteractTick;
+            // _input.offInteract     += OnInteractCancel;
+            // _input.onSprintChanged += OnSprintChanged;
         }
         
-        void Update()
+        private void Update()
         {
-            // 입력이 있을시 이동 모듈에서 입력 Vector2 값을 전달해 이동 함수 호출
-            // 추후 반응형으로 대체( 프로퍼티 )
+            // TODO: BattleInputReader에 onSprintChanged 이벤트 추가 후 이벤트 구독으로 전환
+            // 다른 팀원의 BattleInputReader 작업이 마무리되면 박언약과 합의하여 변경 예정
+            if (_input == null || _movement == null) return;
+            
+            bool currentSprint = _input.isSprint;
+            if (currentSprint != _lastSprintState)
+            {
+                // _movement.SetSprint(currentSprint);
+                _lastSprintState = currentSprint;
+            }
         }
         
         // 이벤트 할당 함수(람다식 연결)
-        // void OnMove(Vector2 v);
-        // void OnJump();
-        // void OnAttack();
-        // void OnInteractStart();
-        // void OnInteractTick();
-        // void OnInteractCancel();
+        void OnMove(Vector2 v) => _movement.SetMoveInput(v);
+        private void OnJump() => _movement?.RequestJump();
+        private void OnSprintChanged(bool b) => _movement?.SetSprint(b);
+        
+        // private void OnAttack() => _combat?.RequestAttack();
+        // private void OnInteractStart() => _interactor?.OnInteractStart();
+        // private void OnInteractTick() => _interactor?.OnInteractTick();
+        // private void OnInteractCancel() => _interactor?.OnInteractCancel();
     }
 }
